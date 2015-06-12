32cf420f6016576228ca71bac3e775fae2b8ed59
Michael Kronk <mkronk7@gmail.com>
2015-06-12 08:30:39 -0400

    continued work on iterating through alphabet string and wrapping each letter in a link tag

--- newmockup/lookup.js
+++ newmockup/lookup.js
@@ -17,11 +17,19 @@ directoryView.prototype.initDirectory = function () {
 	// Find alphabet and make each letter a link for searching
 	var alphabetLinks = $( ".alphabet-links > h5" ).text();
 	console.log( alphabetLinks );
-	//Iterate through structure, making each element a link (skip whitespaces here)
+
+	// Iterate through structure, making each element a link (skip whitespaces here)
+	// See if there is a jquery selector for iterating through a string like this - otherwise, try createElement/insertBefore
 	for (var i = 0; i < alphabetLinks.length; i++) {
 		console.log(alphabetLinks[i]);
+		//$("alphabetLinks[i]").wrap("<a href=\"www.google.com\"></a>");
 	}
 
+	/*
+	$(".alphabet-links > h5").each(function (i) {
+		
+	});
+	*/
 };
 
 // Initialize directory view
