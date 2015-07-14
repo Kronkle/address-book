7be35e908bb01f6301f5003538bbd72edbd00a97
Michael Kronk <mkronk7@gmail.com>
2015-07-14 08:05:50 -0400

    setup dependencies and method for validating password in login.js

--- passport/login.js
+++ passport/login.js
@@ -1,21 +1,36 @@
-passport.use('login', new LocalStrategy({
-	passReqToCallback: true
-	},
-	function(req, username, password, done) {
-		// verify that username exists in mongo
-		User.findOne({ 'username' : username },
-			function(err, user) {
-				if (err)
-					return done(err);
-				if (!user) {
-					console.log('Username not found: ' + username);
-					return done(null, false, req.flash('message', 'User not found'));
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../newmockup/user');
+var bCrypt = require('bcrypt-nodejs');
+
+module.exports = function(passport){
+
+	passport.use('login', new LocalStrategy({
+		passReqToCallback: true
+		},
+		function(req, username, password, done) {
+			// verify that username exists in mongo
+			User.findOne({ 'username' : username },
+				function(err, user) {
+					if (err)
+						return done(err);
+					if (!user) {
+						console.log('Username not found: ' + username);
+						return done(null, false, req.flash('message', 'User not found'));
+					}
+					if (!isValidPassword(user, password)){
+						console.log('Incorrect password entered');
+						return done(null, false, req.flash('message', 'Invalid Password'));
+					}
+					// Username is found and correct password entered
+					return done(null, user);
 				}
-				if (!isValidPassword(user, password)){
-					console.log('Incorrect password entered');
-					return done(null, false, req.flash('message', 'Invalid Password'));
-				}
-				// Username is found and correct password entered
-				return done(null, user);
-			});
-}));
+			);
+		}
+	));
+
+	var isValidPassword = function(user, password){
+		return bCrypt.compareSync(password, user.password);
+	}
+
+}
+
